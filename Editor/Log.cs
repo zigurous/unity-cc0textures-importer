33daf6dc8b6449495d399708d622f4ad7bb986d4
Adam Graham <adam@zigurous.com>
2022-02-17 23:28:49 -0600

    Change debug log format

--- Editor/Log.cs
+++ Editor/Log.cs
@@ -4,21 +4,21 @@ namespace Zigurous.Importer.CC0Textures
 {
     internal static class Log
     {
-        public static Object context;
+        internal static Object context;
 
-        public static void Message(string message)
+        internal static void Message(string message)
         {
-            Debug.Log("[CC0TexturesImporter]: " + message, context);
+            Debug.Log("[CC0TexturesImporter] " + message, context);
         }
 
-        public static void Warning(string warning)
+        internal static void Warning(string warning)
         {
-            Debug.LogWarning("[CC0TexturesImporter]: " + warning, context);
+            Debug.LogWarning("[CC0TexturesImporter] " + warning, context);
         }
 
-        public static void Error(string error)
+        internal static void Error(string error)
         {
-            Debug.LogError("[CC0TexturesImporter]: " + error, context);
+            Debug.LogError("[CC0TexturesImporter] " + error, context);
         }
 
     }
